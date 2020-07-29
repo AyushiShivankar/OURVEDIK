6c8815fb0d8460c170543a45795948674c5710ed
AyushiShivankar <ayushishivankar@gmail.com>
2020-07-28 23:04:49 -1100

    final build

---
+++ app/src/main/java/com/example/ourvedik/EnterOTP.kt
@@ -0,0 +1,11 @@
+package com.example.ourvedik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterOTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_o_t_p)
+    }
+}
---
+++ app/src/main/java/com/example/ourvedik/Home.kt
@@ -0,0 +1,13 @@
+package com.example.ourvedik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
